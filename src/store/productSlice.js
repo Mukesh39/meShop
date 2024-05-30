c80c844c9c3e3a115a8a5a42918f354ee1da64f9
Mukesh Kumar <muk.786422@gmail.com>
2024-05-31 01:01:44 +0530

    code cleaning

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -18,18 +18,6 @@ const productSlice = createSlice({
   },
 
   reducers: {},
-  // reducers: {
-  //   setProducts(state, action) {
-  //     state.data = action.payload;
-  //   },
-
-  //   setStatus(state, action) {
-  //     state.status = action.payload;
-  //   },
-  // },
-  // remove(state, action) {
-  //   return state.filter((item) => item.id !== action.payload);
-  // },
 
   extraReducers: (builder) => {
     builder
@@ -52,33 +40,6 @@ export const { setProducts, setStatus } = productSlice.actions;
 
 export default productSlice.reducer;
 
-//Thunk
-// this is one way to handle the Asynchronous Operation in redux we have some otehr methdos in specially in redyx toolkits
-
-// export function fetchProducts() {
-//   return async function fetchProductsThunk(dispatch, getState) {
-//     dispatch(setStatus(STATUSES.LOADING));
-
-//     try {
-//       const res = await fetch("https://fakestoreapi.com/products");
-
-//       const data = await res.json();
-
-//       console.log(data);
-
-//       dispatch(setProducts(data));
-
-//       dispatch(setStatus(STATUSES.IDLE));
-//     } catch (error) {
-//       console.log(error);
-
-//       dispatch(setStatus(STATUSES.ERROR));
-//     }
-//   };
-// }
-
-// 2nd way to deal with Thunk and with Redux toolkits  in redux
-
 export const fetchProducts = createAsyncThunk("products/fetch", async () => {
   try {
     const res = await fetch("https://fakestoreapi.com/products");
@@ -91,5 +52,3 @@ export const fetchProducts = createAsyncThunk("products/fetch", async () => {
     throw error;
   }
 });
-
-// A function that accepts an initial state, an object of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state.
